485f4bab07382ecd70958b90514a76ca24f7519c
fmotta <fmotta@ALUGA-061560>
2023-11-23 11:39:16 -0300

    Adicionar arquivos de projeto.

---
+++ SQS.Consumidor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+//var cliente = new AmazonSQSClient(RegionEndpoint.USEast1);
+var client = new AmazonSQSClient();
+
+//pegando a url da fila...
+var queueUrlResponse = await client.GetQueueUrlAsync("fernando-test");
+
+var request = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl
+};
+
+
+
+while (true)
+{
+    var response = await client.ReceiveMessageAsync(request);
+
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine(message.Body);
+
+        //tirar da fila
+        await client.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle);
+    }
+
+}
+
---
+++ SQS.Fornecedor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+//var cliente = new AmazonSQSClient(RegionEndpoint.USEast1);
+var client = new AmazonSQSClient();
+
+//pegando a url da fila...
+var queueUrlResponse = await client.GetQueueUrlAsync("fernando-test");
+
+var request = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = "teste 123"
+};
+
+await client.SendMessageAsync(request);
